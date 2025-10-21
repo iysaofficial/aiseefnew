707501afcb0c0dda81301ce614d67cde1d34cb22
iysaofficial <youngscientist.iysa@gmail.com>
2025-10-21 09:46:51 +0700

    link yt -k

--- src/components/HomeOwlSlider.js
+++ src/components/HomeOwlSlider.js
@@ -136,7 +136,7 @@ const HomeOwlSlider = () => {
               </h2>
 
               <a
-                href="https://youtu.be/iUgvM_VA1U8"
+                href="https://youtu.be/xA5kvu-72RU?si=K0pRjFolVOR4-aT5"
                 rel="noreferrer noopener"
                 target="_blank"
                 className="site-button m-r10 white button-lg"
@@ -181,7 +181,7 @@ const HomeOwlSlider = () => {
               </h2>
 
               <a
-                href="https://youtu.be/iUgvM_VA1U8"
+                href="https://youtu.be/xA5kvu-72RU?si=K0pRjFolVOR4-aT5"
                 rel="noreferrer noopener"
                 target="_blank"
                 className="site-button m-r10 white button-lg"
